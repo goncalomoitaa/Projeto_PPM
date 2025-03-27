3786174a0a53ec18c860b1b3b2d1540aa1fd2221
tiagomcalves <271846427+tiagomcalves@users.noreply.github.com>
2025-03-27 01:30:51 +0000

    Add T3 solution

--- src/main/scala/AtariGo.scala
+++ src/main/scala/AtariGo.scala
@@ -1,4 +1,5 @@
 import AtariGo.Stone.Stone
+import scala.util.Random
 
 object AtariGo {
 
@@ -10,32 +11,24 @@ object AtariGo {
 //    type Captured = Boolean ????
     val Black, White, Empty = Value
   }
-
-  trait Random {                         //interface Random
-    def nextInt(x : Int) : (Int, Random)
-  }
-
+    
   case class MyRandom(seed : Long) extends Random {
     def nextInt(x : Int): (Int , Random) = {
       val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
       val nextRandom = MyRandom(newSeed)
-      val n = (newSeed >>> 16).toInt % x                           //x é o tamanho máximo permitido
-      (if (n < 0) -n else n, nextRandom)                             //caso o n seja negativo transforma em positivo
+      val n = (newSeed >>> 16).toInt % x        //x é o tamanho máximo permitido
+      (if (n < 0) -n else n, nextRandom)        //caso o n seja negativo transforma em positivo
     }
   }
-
-  def randomMove(lstOpenCoords : List[Coord2D], rand : Random) : (Coord2D, Random) = {
+  //  T1
+  def randomMove(lstOpenCoords : List[Coord2D], rand : MyRandom) : (Coord2D, MyRandom) = {
     val (randInt, newRand) = rand.nextInt(lstOpenCoords.length)
     (lstOpenCoords(randInt), newRand)
   }
 
   def isElementUnused(coord : Coord2D, lstOpenCoords : List[Coord2D]) : Boolean = {
-    (lstOpenCoords foldRight false)((x, r) =>
-      if(x == coord)
-        true
-      else
-        r
-    )
+    (lstOpenCoords foldRight false)(
+      (x, r) => if(x == coord) true else r )
   }
 
   def lengthBoard(board : Board): Int = {
@@ -44,17 +37,18 @@ object AtariGo {
 
   def filterBoard(lstOpenCoords : List[Coord2D], coord : Coord2D) : List[Coord2D] = lstOpenCoords match {
     case Nil => Nil
-    case primeiro :: tail =>
-      if(primeiro == coord)
+    case head :: tail =>
+      if(head == coord)
         filterBoard(tail, coord)
       else
-        primeiro :: filterBoard(tail, coord)
+        head :: filterBoard(tail, coord)
   }
   
   //perguntar se podemos usar filternot???
+  //  T2
   def play(board : Board, player : Stone, coord : Coord2D, lstOpenCoords : List[Coord2D]) : (Option[Board], List[Coord2D]) = {
     if(!isElementUnused(coord, lstOpenCoords))
-      (None, lstOpenCoords)
+      (None, lstOpenCoords) // devolve None porque a jogada é invalida
     else
       val (row, column) = coord
       var newBoard = List.fill(lengthBoard(board), lengthBoard(board))(Stone.Empty)
@@ -62,6 +56,20 @@ object AtariGo {
       (Some(newBoard), filterBoard(lstOpenCoords, coord))
   }
 
+  //  T3
+  //  prof diz que podemos adicionar a posicao que foi jogada na tuple retornada.
+  //  util para implementar 'Undo'
+  def playRandomly(board:Board, r:MyRandom, player:Stone, lstOpenCoords:List[Coord2D],
+                   f:(List[Coord2D], MyRandom) => (Coord2D,MyRandom)
+                  ) : (Board,MyRandom,List[Coord2D]) =
+  {
+//        val (newCoord2D, MyRandomInstance) = randomMove(lstOpenCoords, r)
+    val (newCoord2D, myRandomInstance) = f(lstOpenCoords, r)
+    val (Some(newBoardState), updatedLstOpenCoords) = play(board, player, newCoord2D, lstOpenCoords)
+    (newBoardState, myRandomInstance, updatedLstOpenCoords)
+    
+  }
+  
   def main(args : Array[String]) : Unit = {
     val k = List((0,0), (0,1), (1,1))
     val c = (1,1)
