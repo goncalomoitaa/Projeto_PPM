f423b5968c0f6823fc55d5c6ccb695e1dbb01b4a
tiagomcalves <271846427+tiagomcalves@users.noreply.github.com>
2025-05-19 22:01:04 +0100

    Fix not being able to click a circle

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -1,30 +1,42 @@
 import javafx.fxml.FXML
 import javafx.scene.control.{Button, Label, TextField}
+import javafx.scene.shape.Circle
+import javafx.scene.input.{ MouseEvent, TouchEvent }
 
 class Controller {
+//    @FXML
+//    private var button1: Button = _
+//    @FXML
+//    private var textField1: TextField = _
+//    @FXML
+//    private var clickmebutton: Button = _
+//    @FXML
+//    private var label1: Label = _
     @FXML
-    private var button1: Button = _
-    @FXML
-    private var textField1: TextField = _
-    @FXML
-    private var clickmebutton: Button = _
-    @FXML
-    private var label1: Label = _
+    private var topLeftCircle : Circle = _
     
+    def OnMouseClickedCircle(event: MouseEvent): Unit = {
+        println("Mouse clicked!")
+    }
     
-    def onButtonClickMeWasClicked(): Unit = {
-        println("Hello World")
+    def OnTouchCircle(event: TouchEvent): Unit = {
+        println("Touch pressed!")
     }
-
-    def onButton1Clicked(): Unit = {
-        label1.setText("18cm")
-    }
-
-    def onMouseHoverButton1() : Unit ={
-        label1.setVisible(true)
-    }
-
-    def onMouseLeaveButton1() : Unit ={
-        label1.setVisible(false)
-    }
+    
+//
+//    def onButtonClickMeWasClicked(): Unit = {
+//        println("Hello World")
+//    }
+//
+//    def onButton1Clicked(): Unit = {
+//        label1.setText("18cm")
+//    }
+//
+//    def onMouseHoverButton1() : Unit ={
+//        label1.setVisible(true)
+//    }
+//
+//    def onMouseLeaveButton1() : Unit ={
+//        label1.setVisible(false)
+//    }
 }
