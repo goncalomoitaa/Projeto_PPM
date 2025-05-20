fc8449bec1f400fec110064b7623796b33414a4c
tiagomcalves <271846427+tiagomcalves@users.noreply.github.com>
2025-05-20 18:30:00 +0100

    Add title menu screen

--- src/main/scala/Menu.scala
+++ src/main/scala/Menu.scala
@@ -1,25 +1,34 @@
-import javafx.fxml.{FXML, Initializable}
-import javafx.scene.control.{Button, Label, TextField}
-import javafx.scene.shape.{Circle, StrokeType}
-import javafx.scene.input.{MouseEvent, TouchEvent}
+import javafx.fxml.{ FXML, Initializable }
+import javafx.scene.control.{ Button, Label, RadioButton, TextField, ToggleGroup }
+import javafx.scene.shape.{ Circle, StrokeType }
+import javafx.scene.input.{ MouseEvent, TouchEvent }
 import javafx.scene.layout.GridPane
 import javafx.scene.paint.Color
 import javafx.scene.input.MouseEvent
 import javafx.scene.layout.GridPane
+
 import java.net.URL
 import java.util.ResourceBundle
-import TUI_Utils._
-import AtariGo._
 
 class Menu extends Initializable {
-    @FXML
-    private var boardGrid : GridPane = _
-    private var playerColor: Color = _
-    private var botColor: Color = _
-    private var gameState: GameState = _
-
+    @FXML private var boardGrid : GridPane = _
+    @FXML private var playerColor: Color = _
+    @FXML private var botColor: Color = _
+    @FXML private var gameState: GameState = _
+    @FXML private var maxCapturesField:TextField = _
+    @FXML private var turnTimeField:TextField = _
+    @FXML private var teamGroup:ToggleGroup = _
+    @FXML private var whiteRadio:RadioButton = _
+    @FXML private var blackRadio:RadioButton = _
+    @FXML private var startButton:Button = _
+    
     @FXML
     override def initialize(url: URL, resourceBundle: ResourceBundle): Unit = {
       
+    }
+    
+    @FXML
+    def onStartGame(): Unit = {
+        println("Button On Action triggered: game started!")
     }
 }
